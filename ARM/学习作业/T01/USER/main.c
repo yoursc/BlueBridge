f3dcd94480c402f26bcd942e468f1ed64dd765ff
YOURS-NOTE\yoursc <peiwencao@vip.qq.com>
2018-03-28 10:27:50 +0800

    修改：完善按键功能

--- ARM/学习作业/T01/USER/main.c
+++ ARM/学习作业/T01/USER/main.c
@@ -14,10 +14,10 @@
 
 uint32_t msTimer;
 uint8_t PA1_ON=0,PA2_ON=0;    //两输出口开关标志位
-uint8_t PWM_PA1=90,PWM_PA2=0;  //pwm波占空比设定值 0 - 100
-uint8_t t_h=23,t_m=59,t_s=50;  //时间临时变量
+uint8_t PWM_PA1=90,PWM_PA2=0; //pwm波占空比设定值 0 - 100
+uint8_t t_h=23,t_m=59,t_s=50; //时间临时变量
+uint8_t KEY_IN=0x00;             //按键待处理标志位
 
-void Init_all(void);
 void Delay_ms(uint16_t time_ms);
 void Init_key(void);
 void Init_led(void);
@@ -26,7 +26,10 @@ void Init_lcd(void);
 void Init_pwm(void);
 void Init_usert(void);
 void Init_clock(void);
+void Init_all(void);
 void Show(void);
+void Scan_key(void);
+void Handle_key(void);
 
 /***********************/
 /********主函数*********/
@@ -35,23 +38,18 @@ int main(void)
 {
 	Init_all();
 	Show();
-  while (1)
+	while (1)
   {
+		Scan_key();
+		if(KEY_IN != 0x00){
+			Handle_key();
+		}
   }
 }
 
 /*************************/
 /********子函数区*********/
 /*************************/
-void Init_all(void){
-	SysTick_Config(SystemCoreClock/1000);
-	Init_key();
-	Init_led();
-	Init_pwm();
-	Init_lcd();
-	Init_usert();
-	Init_clock();
-}
 void Delay_ms(uint16_t time_ms){
 	msTimer=time_ms;
 	while(msTimer);
@@ -59,19 +57,14 @@ void Delay_ms(uint16_t time_ms){
 void Init_key(void){
 	/* 按键  B1   B2   B3   B4  共地
 	** 接口 PA0  PA8  PB1  PB2    */
-	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitTypeDef  GPIO_InitStruct;
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA , ENABLE );
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB , ENABLE );
-
-	
+	/***按键上拉输入***/
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_0|GPIO_Pin_8;
-	GPIO_InitStruct.GPIO_Speed= GPIO_Speed_2MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStruct);
-	
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_1|GPIO_Pin_2;
-	GPIO_InitStruct.GPIO_Speed= GPIO_Speed_2MHz;
 	GPIO_Init(GPIOB,&GPIO_InitStruct);
 }
 void Init_led(void){
@@ -80,12 +73,15 @@ void Init_led(void){
 	GPIO_InitTypeDef GPIO_InitStruct;
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC, ENABLE );
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOD, ENABLE );
+	/***LED灯推挽输出***/
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStruct.GPIO_Pin  = LED1 | LED2 | LED3| LED4 | LED5 | LED6 | LED7 | LED8;
 	GPIO_InitStruct.GPIO_Speed= GPIO_Speed_2MHz;
 	GPIO_Init(GPIOC,&GPIO_InitStruct);
+	/***LED灯控制锁存器***/
 	GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_2;
 	GPIO_Init(GPIOD,&GPIO_InitStruct);
+	
 	LED_Ctrl(LEDALL,0);
 }
 void LED_Ctrl(uint16_t ledx,uint8_t led_status){
@@ -111,7 +107,16 @@ void Init_usert(void)
 void Init_clock(void)
 {
 }
-void Show(void){
+void Init_all(void){
+	SysTick_Config(SystemCoreClock/1000);
+	Init_key();
+	Init_led();
+	Init_pwm();
+	Init_lcd();
+	Init_usert();
+	Init_clock();
+}
+void Show(void){//接收串口数据显示尚未更改
 	uint8_t  string[20],user_string[20];
 	
 	sprintf(string," PWM_PA1:%3d%%",PWM_PA1);
@@ -129,4 +134,49 @@ void Show(void){
 	LCD_DisplayStringLine(Line7,user_string);
 }
 
+void Scan_key(void){
+	uint16_t Deviation_ms = 30;
+	if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0) == 0){
+		Delay_ms(Deviation_ms);
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0) == 0)
+			KEY_IN = 0x01;
+	}
+	if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_8) == 0){
+		Delay_ms(Deviation_ms);
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_8) == 0)
+			KEY_IN = 0x02;
+	}
+	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1) == 0){
+		Delay_ms(Deviation_ms);
+		if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1) == 0)
+			KEY_IN = 0x03;
+	}
+	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_2) == 0){
+		Delay_ms(Deviation_ms);
+		if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_2) == 0)
+			KEY_IN = 0x04;
+	}
+}
+void Handle_key(void){
+	switch(KEY_IN){
+		case 0x01:
+			PA1_ON = ~PA1_ON;
+			break;
+		case 0x02:
+			PA2_ON = ~PA2_ON;
+			break;
+		case 0x03:
+			PWM_PA1+=10;
+			if(PWM_PA1>100)
+				PWM_PA1=0;
+			break;
+		case 0x04:
+			PWM_PA2+=10;
+			if(PWM_PA2>100)
+				PWM_PA2=0;
+			break;
+	}
+	Show();
+	KEY_IN=0x00;
+}
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
