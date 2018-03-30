312516ce805ff77eaf4af037960d48610d5a374d
YOURS-NOTE\yoursc <peiwencao@vip.qq.com>
2018-03-30 16:10:57 +0800

    修改：添加KEY模块

--- ARM/学习作业/T02/USER/main.c
+++ ARM/学习作业/T02/USER/main.c
@@ -1,16 +1,30 @@
 #include "stm32f10x.h"
 #include <stdio.h>
 #include "led.h"
+#include "key.h"
+
+uint32_t msTimer;
+
 
 int main(void)
 {
+	SysTick_Config(SystemCoreClock/1000);
 	Init_Led();
+	KEY_Init();
 	LED_Control(LEDALL,0);
 	LED_Control(LED1,1);
   while (1)
   {
+		if(Key_Scan()==0x02)
+		{
+			LED_Control(LED2,1);
+		}
   }
 }
 
+void Delay_ms(uint32_t xMs){
+	msTimer=xMs;
+	while(msTimer);
+}
 
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
