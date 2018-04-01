cc8c66a0edaa77a6c2e9c1be345e64be0647fe54
YOURS-NOTE\yoursc <peiwencao@vip.qq.com>
2018-04-01 18:20:03 +0800

    新建工程

--- ARM/学习作业/2018ARM/USER/main.c
+++ ARM/学习作业/2018ARM/USER/main.c
@@ -1,9 +1,27 @@
 #include "stm32f10x.h"
 #include <stdio.h>
+#include "i2c.h"
+#include "lcd.h"
 
+//自建头文件
+#include "led.h"
+
+//辅助变量
+
+
+//核心变量
+
+
+//函数声明
+u32 TimingDelay = 0;
+
+//*****************************//
+//**********主函数*************//
+//*****************************//
 int main(void)
 {
- 
+	SysTick_Config(SystemCoreClock/1000);
+
   while (1)
   {
   }
